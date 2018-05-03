ea32c0cd78a9097e6fca3e03c695f87d0bd13171
Alex <vassilyevalexey@gmail.com>
2018-05-03 15:16:59 +0600

    ++

--- Module8/Program.cs
+++ Module8/Program.cs
@@ -6,27 +6,42 @@ using System.Threading.Tasks;
 
 namespace Module8
 {
+    /*
+      1.	Создать индексатор для одномерного массива который при установке значения будет
+      возводить в квадрат передаваемое значение переменной и устанавливать его для указанного индекса.
+      При получении элемента массива по индексу будет возвращаться его текущее значение.
+      */
     class Program
     {
         public class Nuum
         {
-            public int [] Num { get; set; }
-        }
+            public Random rand = new Random();
+            public int[] Num { get; set; }
 
-        public class Result
-        {
-           
-
-            public List<Nuum> LisNum { get; set; }
-
-            public Nuum this[int Index]
+            public Nuum()
             {
-                set => LisNum[Index] = value;
+                Num = new int[rand.Next(0, 10)];
+            }
+
+            public int this[int index]
+            {
+                get
+                {
+                    return Num[index];
+                }
+
+                set
+                {
+                    Num[index] = value * value;
+                }
             }
         }
 
         static void Main(string[] args)
         {
+            Nuum num = new Nuum();
+            num[0] = 22;
+            Console.WriteLine(num[0]);
         }
     }
 }
